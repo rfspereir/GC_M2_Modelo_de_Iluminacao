bb04cd42923996102f9d621c86349dedb5458395
Rafael Pereira <contato@rafael-pereira.com>
2024-05-05 21:42:58 -0300

    Menu Inicial - Ok

--- Assets/Raycast.cs
+++ Assets/Raycast.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Raycast : MonoBehaviour
 {
@@ -13,6 +14,10 @@ public class Raycast : MonoBehaviour
     public float specular;
     [Range(0,1)]
     public float ambient;
+    //public Button renderButton;
+    public bool autoUpdate;
+    //public Toggle autoUpdateToggle;
+    public Color ambientColor;
 
     
     void Start()
@@ -21,19 +26,41 @@ public class Raycast : MonoBehaviour
         Renderer rend = GetComponent<Renderer>();
         tex= new Texture2D(50, 50);
         tex.filterMode = FilterMode.Trilinear;
-        rend.material.mainTexture = tex;            
+        rend.material.mainTexture = tex;
+        ambientColor = EditorGUILayout.ColorField("Ambient Color", ambientColor);      
+        
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)){
-            StartCoroutine("RenderScene");
-        }
+        // if (Input.GetKeyDown(KeyCode.Space)){
+        //     StartCoroutine("RenderScene");
+        // }
         Debug.DrawRay(ray.origin, ray.direction* 5.0f, Color.red);
-        
+    }
+    public void RenderButtonClicked()
+    {
+        ClearTexture(); // Limpa a textura
+        StartCoroutine(RenderScene());
     }
 
-    IEnumerator RenderScene(){
+    public void ClearTexture()
+{
+    Color[] clearPixels = new Color[tex.width * tex.height]; // Cria um array de cores vazio
+    for (int i = 0; i < clearPixels.Length; i++)
+    {
+        clearPixels[i] = Color.white; // Define todas as cores como branco para limpar a textura
+    }
+    tex.SetPixels(clearPixels); // Define os pixels da textura como branco
+    tex.Apply(); // Aplica as alterações na textura
+}
+
+ void ToggleAutoUpdate(bool newValue)
+    {
+        autoUpdate = newValue;
+    }
+
+    public IEnumerator RenderScene(){
         for (int y = 0; y < tex.height; y++)
         {
             for (int x = 0; x < tex.width; x++)
---
+++ Assets/RaycastEditor.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+[CustomEditor(typeof(Raycast))] // Alterado para o nome do seu script, Raycast
+public class RaycastEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        Raycast raycast = (Raycast)target;
+
+        if (DrawDefaultInspector())
+        {
+            if (raycast.autoUpdate)
+            {
+                raycast.RenderButtonClicked(); // Alterado para chamar diretamente a função RenderScene()
+            }
+        }
+
+        if (GUILayout.Button("Render")) // Alterado o texto do botão para "Render" em vez de "Generate"
+        {
+            raycast.RenderButtonClicked(); // Alterado para chamar diretamente a função RenderScene()
+        }
+    }
+}
